b05b94d7707ce7726e9f3bf9834d2495aa50a20d
ChenSingYo <chensingyo@gmail.com>
2020-12-29 18:35:56 +0800

    Revert "refactor: modify mongoose config"
    This reverts commit 3446d29b2713925caa6afe6903b8093e44702784.

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose')
 const restaurantModel = require('../restaurantModel') // import restaurantModel.js
 const restaurantList = require('./restaurant.json') // import seed data
 
-const db = require('../../config/mongoose')
+mongoose.connect('mongodb://localhost:27017/restaurants', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
 
 db.once('open', () => {
+  console.log('mongodb connected!')
   for (let i = 0; i < restaurantList.results.length; i++) {
     restaurantModel.create(restaurantList.results[i])
   }
