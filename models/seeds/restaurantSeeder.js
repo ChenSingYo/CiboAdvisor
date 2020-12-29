133e676ede3f50edf268f754c518b9ecf0c7d28e
ChenSingYo <chensingyo@gmail.com>
2020-12-30 01:40:08 +0800

    style: search bar UI

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,20 +1,8 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const restaurantModel = require('../restaurantModel') // import restaurantModel.js
 const restaurantList = require('./restaurant.json') // import seed data
 
-mongoose.connect('mongodb://localhost:27017/restaurants', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < restaurantList.results.length; i++) {
     restaurantModel.create(restaurantList.results[i])
   }
