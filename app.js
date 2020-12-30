d73a4da6b5e65f83bcdf90eccacac96f66c9f3fd
ChenSingYo <chensingyo@gmail.com>
2020-12-30 11:44:48 +0800

    refactor: arrange code order

--- app.js
+++ app.js
@@ -4,29 +4,32 @@ const app = express()
 const port = 3000
 // require express-handlebars here
 const exphbs = require('express-handlebars')
-
 // import handlebars 'is' helper
 const helpers = require('handlebars-helpers')()
 const helperIs = helpers.is()
-
-// 載入 method-override
+// import method-override
 const methodOverride = require('method-override')
-
-// 引用路由器
+// import routes
 const routes = require('./routes')
 
 // setting template engine and handlebars-helpers
 app.engine('handlebars', exphbs({ helpers: helperIs, defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 // setting static files
 app.use(express.static('public'))
+
 require('./config/mongoose')
+
 // use epxress-urlencoded
 app.use(express.urlencoded({ extended: true }))
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
+
+// use method-override
 app.use(methodOverride('_method'))
-// 將 request 導入路由器
+
+// pet requests into routers
 app.use(routes)
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
